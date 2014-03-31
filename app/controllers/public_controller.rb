e68deb01248ab76f1a1c0eb0667b6a393a1a71b5
grayoverride <johan.b.damm@gmail.com>
2014-03-31 20:27:56 +0200

    woop

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,7 +4,8 @@ class PublicController < ApplicationController
 
   def index
   	#@feed = Feedjira::Feed.fetch_and_parse('http://pressword2continue.wordpress.com/feed/')
-  	urls = %w[http://feedjira.com/blog/feed.xml http://pressword2continue.wordpress.com/feed/]
-	feeds = Feedjira::Feed.fetch_and_parse urls # returns a Hash, with each url having a Feedjira::Feed object
+  	#urls = %w[http://feedjira.com/blog/feed.xml http://pressword2continue.wordpress.com/feed/]
+	#feeds = Feedjira::Feed.fetch_and_parse urls # returns a Hash, with each url having a Feedjira::Feed object
+  	#feeds = FeedEntry.update_from_feed("http://feedjira.com/blog/feed.xml")
   end
 end
